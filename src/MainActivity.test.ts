8355bf8ccb636e69e72de4ac0b0e3498451dd276
Jelmer Cormont <jelmer@cormont.work>
2025-01-20 08:47:26 +0800

    Chore: npm upgrade, use vitest

--- src/MainActivity.test.ts
+++ src/MainActivity.test.ts
@@ -1,24 +1,27 @@
-import { describe, test, useTestContext } from "@talla-ui/test-handler";
+import {
+  clickOutputAsync,
+  expectOutputAsync,
+  useTestContext,
+} from "@talla-ui/test-handler";
+import { beforeEach, test } from "vitest";
 import { MainActivity } from "./MainActivity";
 
-describe("MainActivity", (scope) => {
-  let activity: MainActivity;
-  scope.beforeEach(() => {
-    const app = useTestContext();
-    activity = new MainActivity();
-    app.addActivity(activity, true);
-  });
+let activity: MainActivity;
+beforeEach(() => {
+  const app = useTestContext();
+  activity = new MainActivity();
+  app.addActivity(activity, true);
+});
 
-  test("Counter shows 0 at first", async (t) => {
-    await t.expectOutputAsync({ text: "0" });
-  });
+test("Counter shows 0 at first", async (t) => {
+  await expectOutputAsync({ text: "0" });
+});
 
-  test("Counter goes up when button clicked", async (t) => {
-    let btn = await t.clickOutputAsync({
-      type: "button",
-      accessibleLabel: "Increment counter",
-    });
-    btn.click();
-    await t.expectOutputAsync({ text: "2" });
+test("Counter goes up when button clicked", async (t) => {
+  let btn = await clickOutputAsync({
+    type: "button",
+    accessibleLabel: "Increment counter",
   });
+  btn.click();
+  await expectOutputAsync({ text: "2" });
 });
--- test-runner.config.js
+++
@@ -1,4 +0,0 @@
-// add esbuild config here, e.g. loaders for non-TS files
-export default {
-  loader: { ".svg": "text" },
-};
